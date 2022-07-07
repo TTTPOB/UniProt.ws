f88c77f6587668811ff7d932f7fd645faeab852e
TPOB <tpob@tpob.xyz>
2022-07-07 14:14:24 +0800

    Update AllClasses.R

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -90,7 +90,7 @@ setMethod("taxIdUniprots", "UniProt.ws",
 
 ## Helper to retrieve Uniprot IDs and cache them
 .getUniprots <- function(taxId=9606) {
-    url <- 'https://www.uniprot.org/uniprot/?query=organism:'
+    url <- 'https://www.uniprot.org/uniprot/?query=organism_id:'
     idUrl <- paste0(url, taxId, "&format=tab&columns=id")
     ## Now return that data
     readLines(idUrl)[-1]
